7d6a04ec0afd738ba7e416be1e35d3094a626906
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-02-29 01:44:42 -0800

    CHAPTER 4

---
+++ CHAPTER NO 4/app.js
@@ -0,0 +1,22 @@
+var personName = +prompt('ENTER YOUR BANK BALANCE HERE');
+
+if (personName > 150000) {
+     console.log("MALIK RISHTA DONE HY");
+    }
+else {
+    console.log("MALIK RSHTA CANCEL");
+    
+}
+var resultof = +prompt('enter your percentage here');
+
+if (resultof > 100) {
+   console.log(" PERCENTAGE TO SAHIHH DE");
+} else if (resultof >= 80) {
+   console.log("MALIK A GRADE AAYA HY");
+ } else if (resultof >= 50  && resultof <= 60){
+   console.log("BHAI B GRADE AAYA HY");
+ } else if (resultof <= 49) {
+   console.log("MALIK FAIL HY RIKSHAW CHALAO");
+ } else if (resultof >= 80) {
+   console.log("MALIK A GRADE AAYA HY");
+ }
