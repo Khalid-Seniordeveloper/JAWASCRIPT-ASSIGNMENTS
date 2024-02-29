dab12417f43b58cc97e3834fc008f00040c8c947
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-02-29 01:54:38 -0800

    CHAPTER 5

---
+++ chapter no 5/app.js
@@ -0,0 +1,16 @@
+// var num1 = 16 ;
+// var num2 = 32 ;
+// var num3 = 3 ;
+// var  num4 = 4 ;
+
+let num1 = +prompt("ENTER FIRST NUMBER");
+let num2 = +prompt("ENTER SECOND NUMBER");
+let nuM3 = +prompt("ENTER THIRD NUMBER");
+let num4 = +prompt("ENTER FOURTH NUMBER");
+let num5 = +prompt("ENTER Fifth NUMBER");
+
+
+
+console.log(num1 + num2 +nuM3 + num4);
+
+console.log(num1 / num2 - (nuM3 + num4) * num5);
