92ff262964689da82f53b795feefec0cd556e785
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-02-29 10:59:48 -0800

    ELSE , IF

---
+++ CHAPTER NO 6/app.js
@@ -0,0 +1,49 @@
+// if else assignment 
+
+// """// QUESTION NO 1"""
+
+
+var num1 = +prompt("ENTER NUMBER THAT YOU WANTS TO DIVIDE BY 3.");
+ var  number= parseInt(num1);
+
+ if (number % 3 === 0){
+    console.log(number + " " + "NUMBER IS DIVISIBLE BY 3 ")} else {
+        console.log(number + " " + "NUMBER IS NOT DIVISIBLE BY 3");
+    }
+
+
+// QUESTION NO 2
+
+var num2 = +prompt("ENTER YOUR NUMBER HERE TO CHECK ODD OR EVEN");
+
+if (num2 % 2  === 0 ){
+    console.log(num2 + " " + "NUMBER IS EVEN");
+} else {
+    console.log(num2 + " " + "NUMBER IS ODD");
+}
+
+// QUESTION NO 3
+
+
+
+var username= prompt("ENTER YOUR NAME HERE");
+ var num3 = +prompt("ENTER TO CHECK AGE");
+
+if(num3 >= 18){
+    console.log(username +" "+ " IS VERY OLD ENOUHGH");
+} else{
+    console.log(username+ " " + "USER IS TOO YOUNG");
+}
+
+
+
+// QUESTION NO 4 
+// var yourname = prompt("AGAIN ENTER YOUR NAME")
+if (username.toLocaleUpperCase () === "KHALID") {
+    console.log("HEY KHALID WELCOME !")} else{
+        console.log("NIKKAL BETA KHALID KO BHEJ")
+    
+}
+
+
+
