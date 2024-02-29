9e3a0b5724d04546b928b652971cfd8bd457d5b1
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-02-29 00:08:40 -0800

    SECOND CHAPTER

---
+++ CHAPTER NO 2/app.js
@@ -0,0 +1,13 @@
+var age= 15;
+var vist = 14;
+
+
+alert ("hEY I'm khalid i'm" + age +" " + "old")
+alert ("Hey Khalid you visit this site" + " " + vist + " times" );
+
+var visitors_name = "MUHAMMAD KHALID";
+var product_title = "T-SHIRTS";
+var QUANTITY = 10;
+
+
+console.log(visitors_name + " " + "ordered" + " " + QUANTITY + " " +product_title);
